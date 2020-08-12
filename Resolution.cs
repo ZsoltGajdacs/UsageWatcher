dd096478f9075ca9e0e3f9ef3afa0358ca2cd174
Zsolt Gajdács <33376024+ZsoltGajdacs@users.noreply.github.com>
2020-08-12 13:10:43 +0200

    Update Resolution.cs

--- Resolution.cs
+++ Resolution.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 
 namespace UsageWatcher
 {
+    [Serializable]
     public enum Resolution
     {
         HALF_MINUTE = 30000,
         ONE_MINUTE = 60000,
         TWO_MINUTES = 120000,
+        THREE_MINUTES = 180000,
+        FOUR_MINUTES = 240000,
         FIVE_MINUTES = 3000000,
         TEN_MINUTES = 600000,
         FIFTEEN_MINUTES = 900000,
